16df881d4847b4363a8de381c5df74d598fa3bc9
14Phoenix <98857112+14Phoenix@users.noreply.github.com>
2024-02-21 12:18:04 +0100

    Switched to One Hot Encoding.
    Model now uses One Hot Encoding.
    
    NM_Project_Create_Model.py now produces a model that can predict the number of hours from an image with ~95% accuracy.
    
    NM_Project_Evaluate_Model.py now tests model's accuracy on the test set and displays a confusion matrix for it.

--- NM_Project_Create_Model.py
+++ NM_Project_Create_Model.py
@@ -18,15 +18,24 @@ img_size = (300, 300)
 files = next(os.walk(img_dir_path))[2]
 
 from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
 
-input_train, input_test, output_train, output_test = train_test_split(files,
-                                                                      output_data_numpy,
-                                                                      train_size=0.8,
-                                                                      shuffle=True,
-                                                                      random_state=14)
+output_data_numpy_hour = output_data_numpy[:, 0]
+# output_data_numpy_minute = output_data_numpy[:, 1]
 
-train_dataset = tf.data.Dataset.from_tensor_slices((input_train, output_train))
-test_dataset = tf.data.Dataset.from_tensor_slices((input_test, output_test))
+output_data_numpy_hour_OHE = to_categorical(output_data_numpy_hour)
+# output_data_numpy_minute_OHE = to_categorical(output_data_numpy_minute)
+
+# output_data_numpy_OHE = np.concatenate((output_data_numpy_hour_OHE, output_data_numpy_minute_OHE), axis=1)
+
+input_train_hour, input_test_hour, output_train_hour_OHE, output_test_hour_OHE = train_test_split(files,
+                                                                                                  output_data_numpy_hour_OHE,
+                                                                                                  train_size=0.8,
+                                                                                                  shuffle=True,
+                                                                                                  random_state=14)
+
+train_dataset = tf.data.Dataset.from_tensor_slices((input_train_hour, output_train_hour_OHE))
+test_dataset = tf.data.Dataset.from_tensor_slices((input_test_hour, output_test_hour_OHE))
 
 def read_image(image_path, lab):
     image = tf.io.read_file(img_dir_path + "\\" + image_path)
@@ -39,33 +48,36 @@ test_dataset = test_dataset.map(read_image).batch(64)
 # Create a model
 from keras import Sequential
 from keras import layers
+from keras.regularizers import l2
 
 model = Sequential([
-    layers.RandomContrast(0.2, input_shape=(300, 300, 3)),
+    layers.RandomContrast(0.1, input_shape=(300, 300, 3)),
     layers.RandomBrightness(0.2),
     layers.Conv2D(16, kernel_size=(3, 3), padding='same', activation='relu'),
     layers.MaxPooling2D(),
     layers.Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu'),
     layers.MaxPooling2D(),
     layers.Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu'),
+    layers.MaxPooling2D(),
     layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(2)
+    layers.Dropout(0.3),
+    layers.Dense(128, activation='relu', kernel_regularizer=l2(0.001)),
+    layers.Dense(12, activation='softmax')
 ])
 
 model.summary()
 
 # Compile and train the model
-from keras.losses import MeanSquaredError
+from keras.losses import BinaryCrossentropy
 from keras.callbacks import EarlyStopping
 
 model.compile(optimizer='adam',
-              loss=MeanSquaredError(),
+              loss=BinaryCrossentropy(),
               metrics=['accuracy'])
 
-stop_early = EarlyStopping(monitor='val_accuracy', patience=3, restore_best_weights=True)
+stop_early = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
 history = model.fit(train_dataset,
-                    epochs=20,
+                    epochs=50,
                     validation_data=test_dataset,
                     callbacks=[stop_early],
                     verbose=1)
@@ -93,4 +105,4 @@ plt.plot(val_loss)
 plt.title('Loss')
 plt.show()
 
-model.save('NM_Project.h5')
+model.save('NM_Project_hour_OHE.h5')
--- NM_Project_Evaluate_Model.py
+++ NM_Project_Evaluate_Model.py
@@ -18,26 +18,50 @@ img_size = (300, 300)
 files = next(os.walk(img_dir_path))[2]
 
 from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
 
-input_train, input_test, output_train, output_test = train_test_split(files,
-                                                                      output_data_numpy,
-                                                                      train_size=0.8,
-                                                                      shuffle=True,
-                                                                      random_state=14)
+output_data_numpy_hour = output_data_numpy[:, 0]
+# output_data_numpy_minute = output_data_numpy[:, 1]
 
-train_dataset = tf.data.Dataset.from_tensor_slices((input_train, output_train))
-test_dataset = tf.data.Dataset.from_tensor_slices((input_test, output_test))
+output_data_numpy_hour_OHE = to_categorical(output_data_numpy_hour)
+# output_data_numpy_minute_OHE = to_categorical(output_data_numpy_minute)
+
+input_train_hour, input_test_hour, output_train_hour_OHE, output_test_hour_OHE = train_test_split(files,
+                                                                                                  output_data_numpy_hour_OHE,
+                                                                                                  train_size=0.8,
+                                                                                                  shuffle=True,
+                                                                                                  random_state=14)
+
+train_dataset_hour = tf.data.Dataset.from_tensor_slices((input_train_hour, output_train_hour_OHE))
+test_dataset_hour = tf.data.Dataset.from_tensor_slices((input_test_hour, output_test_hour_OHE))
 
 def read_image(image_path, lab):
     image = tf.io.read_file(img_dir_path + "\\" + image_path)
     image = tf.image.decode_jpeg(image, channels=3)
     return image, lab
 
-train_dataset = train_dataset.map(read_image).batch(64)
-test_dataset = test_dataset.map(read_image).batch(64)
+train_dataset_hour = train_dataset_hour.map(read_image).batch(64)
+test_dataset_hour = test_dataset_hour.map(read_image).batch(64)
 
 # Load model
-model = tf.keras.models.load_model("NM_Project.h5")
+model = tf.keras.models.load_model("NM_Project_hour_OHE.h5")
 model.summary()
 
-print("Accuracy: " + str(100 * model.evaluate(test_dataset, verbose=1)[1]) + "%")
+# Predict train and test data
+
+test_prediction_hour_OHE = model.predict(test_dataset_hour, verbose=1)
+test_prediction_hour = np.argmax(test_prediction_hour_OHE, axis=1)
+output_test_hour = np.argmax(output_test_hour_OHE, axis=1)
+test_accuracy = np.sum(test_prediction_hour == output_test_hour) / len(output_test_hour)
+print("Model accuracy: " + str(test_accuracy * 100) + "%")
+
+# Confusion matrix
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+# Test confusion matrix
+plt.figure()
+cm = confusion_matrix(output_test_hour, test_prediction_hour, normalize='true')
+cm_display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[i for i in range(12)])
+cm_display.plot()
+plt.show()
+
